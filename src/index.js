659327bc951bbd5262303eb0ea114a0bae91a222
rsneal01 <rsneal01@gmail.com>
2022-02-25 09:18:24 -0800

    commit tree 2c122a3976f5df7fac26b86b5f81fb13f6a8bcf2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-const Home = () => {
-  return (
-    <div>
-      <h1>Home!</h1>
-    </div>
-  );
-};
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import About from "./About";
+import Home from "./Home";
+import Login from "./Login";
+import Navbar from "./Navbar";
 
 ReactDOM.render(
-  <Home />,
+  <Router>
+    <div>
+      <Navbar />
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/login" component={Login} />
+      <Route exact path="/messages" component={Messages} />
+      <Route exact path="/signup" component={Signup} />
+    </div>
+  </Router>,
   document.getElementById('root')
 );
